3ce0453c340c8b34434f024cb9c16652820598f7
carolinaarosendo <carolinaarosendo@gmail.com>
2025-09-30 00:37:28 -0300

    CRUD - Alunos e Professores

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+//Log das requisições
+app.use((req, res, next) => {
+  console.log("-------### LOG da Requisição ###-------");
+  console.log("TIME: ", new Date().toLocaleString());
+  console.log("METODO: ", req.method);
+  console.log("ROTA: ", req.url);
+  next();
+});
 
 //IMPORTAR E MAPEAR ROTAS
+// importar alunos
+const alunosRouter = require('./routes/alunos');
+app.use('/alunos', alunosRouter);
+// importar professores
+const professoresRouter = require('./routes/professores');
+app.use('/professores', professoresRouter);
 
 app.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
